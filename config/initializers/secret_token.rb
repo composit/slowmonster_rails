f34616b33e268fb947163e34bb2a9d67fb518040
Mr. Matt <matt@composition9.com>
2012-08-03 14:09:02 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slowmonster::Application.config.secret_token = 'a9447986711936c92b3cbe7a1d23cf857996e5f4743ad6dbc74c82d36385f5af684686ca52e326c2e70961c97b6615efc8440ed4e732df651bcf6aff977e4322'
