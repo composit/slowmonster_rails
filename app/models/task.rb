c86dc4fb81c0ab358c830d1fa7a7d354123a4269
Mr. Matt <matt@composition9.com>
2012-10-03 05:52:27 -0600

    dates to times

--- app/models/task.rb
+++ app/models/task.rb
@@ -56,7 +56,7 @@ class Task < ActiveRecord::Base
     start_threshold.to_date.upto( end_threshold.to_date ).collect do |task_date|
       {
         date: task_date,
-        value: total_value( task_date, task_date + 1.day )
+        value: total_value( task_date.to_time, ( task_date + 1.day ).to_time )
       }
     end
   end
