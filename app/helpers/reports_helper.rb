17941092bc1a77f33f44d20fcb5969354aa53ddc
Mr. Matt <matt@composition9.com>
2013-10-31 22:02:28 -0600

    tweaks to custom report

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,5 +1,5 @@
 module ReportsHelper
   def amount_done_since(task, time)
-    task.total_value(time) / (Time.zone.now.to_date - time.to_date + 1)
+    task.total_value(time) / (Time.zone.now.to_date - time.to_date)
   end
 end
