dd58b495d0ca9646b5cf4ef0fea161b863d0ae10
Mr. Matt <matt@composition9.com>
2013-10-31 22:16:39 -0600

    remove totals from custom report

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,4 +1,11 @@
 module ReportsHelper
+  def report_color(task, low_amount_expected, high_amount_expected)
+    since_time = 1.week.ago
+    color = 'green'
+    color = 'yellow' if amount_done_since(task, since_time) > low_amount_expected
+    color = 'red' if amount_done_since(task, since_time) > high_amount_expected
+  end
+
   def amount_done_since(task, time)
     task.total_value(time) / (Time.zone.now.to_date - time.to_date)
   end
