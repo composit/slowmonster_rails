b1819b9b7017c31b3d42cbaf46fe3ed677b5e447
Mr. Matt <matt@composition9.com>
2014-03-03 22:36:38 -0700

    update task expected weekly times

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,12 +8,12 @@ class ReportsController < ApplicationController
 
   def custom
     @tasks = []
-    task = Task.find_by_content('exercizer') and @tasks << [task, 5.0]
-    task = Task.find_by_content('meditator') and @tasks << [task, 10.0/3.0]
+    task = Task.find_by_content('exercizer') and @tasks << [task, 3.0]
+    task = Task.find_by_content('meditator') and @tasks << [task, 2.5]
     task = Task.find_by_content('Scout') and @tasks << [task, 40.0]
-    task = Task.find_by_content('musics') and @tasks << [task, 4.5]
-    task = Task.find_by_content('codez') and @tasks << [task, 19.0/3.0]
-    task = Task.find_by_content('Bulley & Andrews') and @tasks << [task, 5.0]
+    task = Task.find_by_content('musics') and @tasks << [task, 3.5]
+    task = Task.find_by_content('codez') and @tasks << [task, 6.0]
+    task = Task.find_by_content('Bulley & Andrews') and @tasks << [task, 3.5]
     @color = 'green'
   end
 end
