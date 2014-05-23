dbb1081b42f97ffce738c7799e956cd0a618fc36
Mr. Matt <matt@composition9.com>
2014-05-22 22:02:14 -0600

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:composit/slowmonster.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, proc { `git rev-parse --abbrev-ref angular`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/u/apps/slowmonster'
